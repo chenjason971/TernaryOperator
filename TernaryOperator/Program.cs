4eca122daab385a35ac89a98ab5ba43d550075e4
Jason Chen <chenjason971@gmail.com>
2022-06-15 11:04:02 -0500

    Finished Project

--- TernaryOperator/Program.cs
+++ TernaryOperator/Program.cs
@@ -10,6 +10,13 @@ namespace TernaryOperator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter how long your peppers are: ");
+
+            double pepperLength = Convert.ToDouble(Console.ReadLine());
+            string message = (pepperLength >= 3.5) ? "They are ready!" : "Wait a little longer";
+            Console.WriteLine(message);
+
+            Console.ReadKey();
         }
     }
 }
